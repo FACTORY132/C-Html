d6c6cee1f545976d38304876f01beb6fbadf2138
FACTORY132 <65250110+FACTORY132@users.noreply.github.com>
2023-11-23 23:23:09 +0700

    Add files via upload

---
+++ include/html/css.h
@@ -0,0 +1,11 @@
+
+
+typedef struct
+{
+    char *tag;
+
+} css;
+
+void write(const char* tag);
+
+void _build_file_(css* csc);
---
+++ src/html.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "..\include\html\html.h"
+
+void write(html *ht, const char *element)
+{
+
+    strcat(ht->element, element);
+}
+
+void _build_file_(html *htmlf)
+{
+
+    remove(htmlf->fileName);
+    FILE *file = fopen64(htmlf->fileName, "a");
+    fwrite(htmlf->element, 1, strlen(htmlf->element), file);
+    fclose(file);
+}
